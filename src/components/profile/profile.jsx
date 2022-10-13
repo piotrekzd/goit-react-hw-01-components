85fbeae52b848cf9264c7a79fab253e5899faa6c
Piotr Zdebski <piotrek.zdebski@gmail.com>
2022-10-13 17:50:42 +0200

    update task-01

---
+++ src/components/profile/profile.jsx
@@ -0,0 +1,3 @@
+import PropTypes from 'prop-types';
+import ProfileCSS from './profile.module.css';
+
--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
+import user from './components/profile/user.json'
+
+import { App } from './App';
+import { Profile } from './components/profile/profile';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
