689bc0ba2b489f00a6071ed02c6def1923e97fb5
Piotr Zdebski <piotrek.zdebski@gmail.com>
2022-10-13 19:03:48 +0200

    update task-01

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,3 +1,39 @@
 import PropTypes from 'prop-types';
 import ProfileCSS from './profile.module.css';
 
+export const Profile = ({ username, tag, location, avatar, stats }) => {
+    return (
+        <div>
+            <div>
+                <img src={avatar} alt='avatar' />
+                <p>{username}</p>
+                <p>@{tag}</p>
+                <p>{location}</p>
+            </div>
+            <ul>
+                {[
+                    [1, 'Followers', stats.followers],
+                    [2, 'Views', stats.views],
+                    [3, 'Likes', stats.likes],
+                ].map(([id, text, value]) => (
+                    <li key={id}>
+                        <span>{text}</span>
+                        <span>{value}</span>
+                    </li>
+                ))};
+            </ul>
+        </div>
+    );
+};
+
+Profile.propTypes = {
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string,
+    stats: PropTypes.shape({
+        followers: PropTypes.number.isRequired,
+        views: PropTypes.number.isRequired,
+        likes: PropTypes.number.isRequired
+    }),
+};
--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
 import './index.css';
 
 import user from './components/profile/user.json'
 
-import { App } from './App';
+import { App } from './components/App';
 import { Profile } from './components/profile/profile';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App>
+      <Profile 
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+        />
+    </App>
   </React.StrictMode>
 );
