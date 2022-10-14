a4f98b2efe762c2cf9dbc8873f712e8db41d42db
Piotr Zdebski <piotrek.zdebski@gmail.com>
2022-10-14 20:37:46 +0200

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import { Profile } from "./profile/profile";
+import { Statistics } from "./stats/statistics";
 
-import user from './profile/user.json';
+import user from './json-data/user.json';
+import data from './json-data/data.json';
 
 export const App = () => {
   return (
@@ -12,6 +14,8 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      </>
+    <Statistics title="Upload stats" stats={data} />;
+    </>
+    
   );
 };
---
+++ src/components/stats/statistics.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+
+export const Statistics = ({ title, stats }) => {
+    const randomHex = () => {
+        return `#${Math.floor(Math.random() * 16777215).toString(16)}`
+    }
+    return (
+        <section>
+            {title && <h2>{title}</h2>}
+            <ul>
+                {stats.map(({ id, label, percents }) => (
+                    <li
+                        key={id}
+                        style={{ backgroundColor: randomHex()}}
+                    >
+                        <span>{label}</span>
+                        <span>{percents}%</span>
+                    </li>
+                ))}
+            </ul>
+        </section>
+    );
+};
+
+Statistics.propTypes = {
+    title: PropTypes.string.isRequired
+}
