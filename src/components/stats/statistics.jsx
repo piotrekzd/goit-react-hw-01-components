dafb6af77f4bfd7a9f2c6e039d83ea3b34e6bf5b
Piotr Zdebski <piotrek.zdebski@gmail.com>
2022-10-16 13:04:10 +0200

    styles stats + friendsList

--- src/components/stats/statistics.jsx
+++ src/components/stats/statistics.jsx
@@ -1,20 +1,22 @@
 import PropTypes from 'prop-types';
+import Statistic from './statistics.module.css'
 
 export const Statistics = ({ title, stats }) => {
     const randomHex = () => {
         return `#${Math.floor(Math.random() * 16777215).toString(16)}`
-    }
+    };
     return (
-        <section>
-            {title && <h2>{title}</h2>}
-            <ul>
+        <section className={Statistic.stats}>
+            {title && <h2 className={Statistic.title}>{title}</h2>}
+            <ul className={Statistic.statsList}>
                 {stats.map(({ id, label, percents }) => (
                     <li
+                        className={Statistic.item}
                         key={id}
                         style={{ backgroundColor: randomHex()}}
                     >
-                        <span>{label}</span>
-                        <span>{percents}%</span>
+                        <span className={Statistic.label}>{label}</span>
+                        <span className={Statistic.percents}>{percents}%</span>
                     </li>
                 ))}
             </ul>
