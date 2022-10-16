a567873deb67b908acbf76b038225480861ef8f5
Piotr Zdebski <piotrek.zdebski@gmail.com>
2022-10-16 13:36:04 +0200

    styles transactionsHistory

--- src/components/transactions/transactions.jsx
+++ src/components/transactions/transactions.jsx
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
+import History from './transactions.module.css';
 
 export const Transactions = ({ items }) => {
     return (
-        <table>
-            <thead>
-                <tr>
+        <table className={History.table}>
+            <thead className={History.tableHead}>
+                <tr className={History.tableRow}>
                     <th>Type</th>
                     <th>Amount</th>
                     <th>Currency</th>
@@ -13,9 +14,9 @@ export const Transactions = ({ items }) => {
             <tbody>
                 {items.map(({ id, type, amount, currency }) => (
                     <tr key={id}>
-                        <td>{type}</td>
-                        <td>{amount}</td>
-                        <td>{currency}</td>
+                        <td className={History.tableItem}>{type}</td>
+                        <td className={History.tableItem}>{amount}</td>
+                        <td className={History.tableItem}>{currency}</td>
                     </tr>
                 ))}
             </tbody>
